30060da85ea24781ca616da8e220b5707778c78f
Danvic Wang <danvic96@hotmail.com>
2019-12-29 12:50:51 +0800

    Init ingos api template

---
+++ template/Ingos.Api.Template/src/Ingos.Api/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
+
+namespace Ingos.Api
+{
+    public class Program
+    {
+        /// <summary>
+        /// Namespace name
+        /// </summary>
+        public static readonly string Namespace = typeof(Program).Namespace;
+
+        /// <summary>
+        /// App Name
+        /// </summary>
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+
+        public static void Main(string[] args)
+        {
+            // Use NLog to catch all errors
+            //
+            var logger = NLogBuilder.ConfigureNLog("Configurations/nlog.config").GetCurrentClassLogger();
+
+            try
+            {
+                logger.Info($"{AppName} configuring web host...");
+                var host = CreateWebHostBuilder(args).Build();
+
+                logger.Info($"{AppName} starting web host...");
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, $"{AppName} Program terminated unexpectedly");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging(logger =>
+                {
+                    logger.ClearProviders();
+                    logger.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
+    }
+}
---
+++ template/Ingos.Api.Template/test/Ingos.Application.Test/UnitTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Ingos.Application.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestApplication()
+        {
+            Assert.Pass();
+        }
+    }
+}
