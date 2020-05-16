79615be8c463fa1c84a042e02e7e43db8a6259ca
Danvic Wang <danvic96@hotmail.com>
2020-05-16 11:49:43 +0800

    Add service inject extension method

---
+++ framework/src/infrastructure/Ingos.Infrastructure.Core/ServiceLifetimes/DependencyInjection/ServiceLifetimeServiceCollectionExtensions.cs
@@ -0,0 +1,93 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file= "ServiceLifetimeServiceCollectionExtensions.cs">
+//     Copyright (c) Danvic.Wang All rights reserved.
+// </copyright>
+// Author: Danvic.Wang
+// Created DateTime: 2020/5/16 9:37:18
+// Modified by:
+// Description: Custom service injection extension method
+//-----------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Ingos.Infrastructure.Core.ServiceLifetimes.Contracts;
+using Ingos.Infrastructure.Core.ServiceLifetimes.Exceptions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ingos.Infrastructure.Core.ServiceLifetimes.DependencyInjection
+{
+    public static class ServiceLifetimeServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Dependency inject custom service
+        /// </summary>
+        /// <param name="services">The instance of <see cref="IServiceCollection" /></param>
+        /// <param name="setupAction">The inject service's config options</param>
+        /// <returns></returns>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            Action<ApplicationServiceOptions> setupAction)
+        {
+            if (setupAction == null)
+                throw new ArgumentNullException(nameof(setupAction));
+
+            var options = new ApplicationServiceOptions();
+            setupAction.Invoke(options);
+
+            return InjectServiceCore(services, options);
+        }
+
+        /// <summary>
+        /// Core method of inject service
+        /// </summary>
+        /// <param name="services">The services collection</param>
+        /// <param name="options">The inject service's config options</param>
+        /// <returns></returns>
+        private static IServiceCollection InjectServiceCore(IServiceCollection services,
+            ApplicationServiceOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            if (!options.Assembly.Any())
+                return services;
+
+            var transient = typeof(ITransient);
+            var scoped = typeof(IScoped);
+            var singleton = typeof(ISingleton);
+
+            foreach (var assembly in options.Assembly)
+            {
+                foreach (var implement in assembly.GetTypes())
+                {
+                    var interfaceType = implement.GetInterfaces();
+
+                    var lifetime = interfaceType
+                        .FirstOrDefault(i => i == transient || i == scoped || i == singleton);
+
+                    if (lifetime == null)
+                        throw new ServiceLifetimeException($"Did not find this {nameof(implement)}'s lifetime");
+
+                    foreach (var service in interfaceType)
+                    {
+                        switch (lifetime.Name)
+                        {
+                            case "ITransient":
+                                services.AddTransient(service, implement);
+                                break;
+
+                            case "IScoped":
+                                services.AddScoped(service, implement);
+                                break;
+
+                            case "ISingleton":
+                                services.AddSingleton(service, implement);
+                                break;
+                        }
+                    }
+                }
+            }
+
+            return services;
+        }
+    }
+}
---
+++ framework/src/infrastructure/Ingos.Infrastructure.Core/ServiceLifetimes/Exceptions/ServiceLifetimeException.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file= "ServiceLifetimeException.cs">
+//     Copyright (c) Danvic.Wang All rights reserved.
+// </copyright>
+// Author: Danvic.Wang
+// Created DateTime: 2020/5/16 10:32:36
+// Modified by:
+// Description: Service inject exception
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace Ingos.Infrastructure.Core.ServiceLifetimes.Exceptions
+{
+    public class ServiceLifetimeException : Exception
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public ServiceLifetimeException()
+        {
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="message">Error message</param>
+        public ServiceLifetimeException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="message">Error message</param>
+        /// <param name="inner">Inner exception</param>
+        public ServiceLifetimeException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
