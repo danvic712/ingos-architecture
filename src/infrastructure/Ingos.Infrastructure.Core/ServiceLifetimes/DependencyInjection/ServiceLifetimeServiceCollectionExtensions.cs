4f4def4e0e6e8fd4645ca92b353bc96921091cdb
Danvic Wang <danvic96@hotmail.com>
2020-12-21 21:39:43 +0800

    Adjust custom service dependency injection implementation

--- src/infrastructure/Ingos.Infrastructure.Core/ServiceLifetimes/DependencyInjection/ServiceLifetimeServiceCollectionExtensions.cs
+++ src/infrastructure/Ingos.Infrastructure.Core/ServiceLifetimes/DependencyInjection/ServiceLifetimeServiceCollectionExtensions.cs
@@ -8,8 +8,11 @@
 // Description: Custom service injection extension method
 //-----------------------------------------------------------------------
 
-using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 using Ingos.Infrastructure.Core.ServiceLifetimes.Contracts;
+using Microsoft.Extensions.DependencyModel;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection
@@ -23,9 +26,16 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <returns></returns>
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            // Get all assemblies
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            // Get assemblies
+            //
+            var libraries = DependencyContext.Default.RuntimeLibraries
+                .Where(i => !i.Serviceable && i.Type != "package");
 
+            var assemblies = libraries
+                .Select(i => AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(i.Name))).ToList();
+
+            // Inject services
+            //
             services.Scan(scan => scan.FromAssemblies(assemblies)
                 .AddClasses(classes
                     => classes.AssignableTo<ITransientService>()
@@ -35,12 +45,12 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddClasses(classes
                     => classes.AssignableTo<IScopedService>()
                 )
-                .As<IScopedService>()
+                .AsImplementedInterfaces()
                 .WithScopedLifetime()
                 .AddClasses(classes
                     => classes.AssignableTo<ISingletonService>()
                 )
-                .As<ISingletonService>()
+                .AsImplementedInterfaces()
                 .WithSingletonLifetime()
             );
 
